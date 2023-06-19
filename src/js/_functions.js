67e43cf3abf3df629cae19ccbff2c45c5b99562d
cat <white.wkval@gmail.com>
2023-06-19 14:56:31 +0300

    Site mobile games

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,86 +1,221 @@
-// Данный файл - лишь собрание подключений готовых компонентов.
-// Рекомендуется создавать отдельный файл в папке components и подключать все там
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelector('.dots');
+const arrowPrev = document.querySelector('.arrow-prev');
+const arrowNext = document.querySelector('.arrow-next');
 
-// Определение операционной системы на мобильных
-import { mobileCheck } from "./functions/mobile-check";
-console.log(mobileCheck())
+for (let i = 0; i < slides.length; i++) {
+  const dot = document.createElement('span');
+  dot.classList.add('dot');
+  dots.appendChild(dot);
+}
 
-// Определение ширины экрана
-// import { isMobile, isTablet, isDesktop } from './functions/check-viewport';
-// if (isDesktop()) {
-//   console.log('...')
-// }
+const dotItems = document.querySelectorAll('.dot');
+let currentSlide = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
-// Троттлинг функции (для ресайза, ввода в инпут, скролла и т.д.)
-// import { throttle } from './functions/throttle';
-// let yourFunc = () => { console.log('throttle') };
-// let func = throttle(yourFunc);
-// window.addEventListener('resize', func);
+slides[currentSlide].style.display = 'flex';
+dotItems[currentSlide].classList.add('active');
 
-// Фикс фулскрин-блоков
-// import './functions/fix-fullheight';
+function changeSlide(index) {
 
-// Реализация бургер-меню
-// import { burger } from './functions/burger';
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (let i = 0; i < dotItems.length; i++) {
+    dotItems[i].classList.remove('active');
+  }
 
-// Реализация остановки скролла (не забудьте вызвать функцию)
-// import { disableScroll } from './functions/disable-scroll';
+  slides[index].style.display = 'flex';
+  dotItems[index].classList.add('active');
+}
 
-// Реализация включения скролла (не забудьте вызвать функцию)
-// import { enableScroll } from './functions/enable-scroll';
 
-// Реализация модального окна
-// import GraphModal from 'graph-modal';
-// const modal = new GraphModal();
+for (let i = 0; i < dotItems.length; i++) {
+  dotItems[i].addEventListener('click', function (e) {
+    const dotIndex = Array.prototype.indexOf.call(dotItems, e.target);
+    changeSlide(dotIndex);
+  });
+}
 
-// Реализация табов
-// import GraphTabs from 'graph-tabs';
-// const tabs = new GraphTabs('tab');
 
-// Получение высоты шапки сайта (не забудьте вызвать функцию)
-// import { getHeaderHeight } from './functions/header-height';
+arrowPrev.addEventListener('click', function () {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  changeSlide(currentSlide);
+});
 
-// Подключение плагина кастом-скролла
-// import 'simplebar';
+arrowNext.addEventListener('click', function () {
+  currentSlide = (currentSlide + 1) % slides.length;
+  changeSlide(currentSlide);
+});
 
-// Подключение плагина для позиционирования тултипов
-// import { createPopper, right} from '@popperjs/core';
-// createPopper(el, tooltip, {
-//   placement: 'right'
-// });
 
-// Подключение свайпера
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-// const swiper = new Swiper(el, {
-//   slidesPerView: 'auto',
-// });
+slider.addEventListener('touchstart', function (e) {
+  touchStartX = e.touches[0].clientX;
+});
 
-// Подключение анимаций по скроллу
-// import AOS from 'aos';
-// AOS.init();
+slider.addEventListener('touchend', function (e) {
+  touchEndX = e.changedTouches[0].clientX;
+  handleSwipe();
+});
 
-// Подключение параллакса блоков при скролле
-// import Rellax from 'rellax';
-// const rellax = new Rellax('.rellax');
 
-// Подключение плавной прокрутки к якорям
-// import SmoothScroll from 'smooth-scroll';
-// const scroll = new SmoothScroll('a[href*="#"]');
+slider.addEventListener('mousedown', function (e) {
+  touchStartX = e.clientX;
+});
 
-// Подключение событий свайпа на мобильных
-// import 'swiped-events';
-// document.addEventListener('swiped', function(e) {
-//   console.log(e.target);
-//   console.log(e.detail);
-//   console.log(e.detail.dir);
-// });
+slider.addEventListener('mouseup', function (e) {
+  touchEndX = e.clientX;
+  handleSwipe();
+});
 
-// import { validateForms } from './functions/validate-forms';
-// const rules1 = [...];
+function handleSwipe() {
+  const swipeThreshold = 50;
+  const touchDiff = touchEndX - touchStartX;
 
-// const afterForm = () => {
-//   console.log('Произошла отправка, тут можно писать любые действия');
-// };
+  if (touchDiff > swipeThreshold) {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    changeSlide(currentSlide);
+  } else if (touchDiff < -swipeThreshold) {
 
-// validateForms('.form-1', rules1, afterForm);
+    currentSlide = (currentSlide + 1) % slides.length;
+    changeSlide(currentSlide);
+  }
+}
+
+const burgerMenu = document.getElementById('burgerMenu');
+const menuList = document.querySelector('.menu-list');
+const menuItems = menuList.getElementsByTagName('a');
+let menuOpen = false;
+
+burgerMenu.addEventListener('click', toggleMenu);
+
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener('click', closeMenu);
+}
+
+function toggleMenu() {
+  const lines = document.getElementsByClassName('line');
+  if (menuOpen) {
+    closeMenu(lines);
+    menuOpen = false;
+  } else {
+    openMenu(lines);
+    menuOpen = true;
+  }
+}
+
+function openMenu(lines) {
+  lines[0].style.transform = 'rotate(45deg) translate(5px, 5px)';
+  lines[1].style.opacity = '0';
+  lines[2].style.transform = 'rotate(-45deg) translate(5px, -5px)';
+  if (window.innerWidth <= 768) {
+    menuList.style.display = 'block';
+  }
+}
+
+function closeMenu() {
+  const lines = document.getElementsByClassName('line');
+  lines[0].style.transform = 'none';
+  lines[1].style.opacity = '1';
+  lines[2].style.transform = 'none';
+  if (window.innerWidth <= 768) {
+    menuList.style.display = 'none';
+    menuOpen = false;
+  }
+}
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
+const input4 = document.getElementById('input4');
+const mobileBlock = document.getElementById('mobile');
+const subscribeBtn = document.getElementById('subscribeBtn');
+
+const validateCode = () => {
+  const code = input1.value + input2.value + input3.value + input4.value;
+  if (code === '4231') {
+    subscribeBtn.style.background = 'linear-gradient(180deg, #32E28E 0%, #19C974 100%)';
+    mobileBlock.style.display = 'block';
+  } else {
+    subscribeBtn.style.backgroundColor = '';
+    mobileBlock.style.display = 'none';
+  }
+};
+
+input1.addEventListener('input', validateCode);
+input2.addEventListener('input', validateCode);
+input3.addEventListener('input', validateCode);
+input4.addEventListener('input', validateCode);
+
+const validatePIN = () => {
+  const pin = input1.value + input2.value + input3.value + input4.value;
+  const pinRegex = /^\d{4}$/;
+
+  if (pinRegex.test(pin)) {
+    if (pin === '4231') {
+      subscribeBtn.disabled = false;
+    }
+  } else {
+    subscribeBtn.disabled = true;
+  }
+};
+
+const moveToNextInput = (currentInput, nextInput) => {
+  if (currentInput.value.length === 1) {
+    nextInput.focus();
+  }
+};
+
+input1.addEventListener('input', () => {
+  validatePIN();
+  moveToNextInput(input1, input2);
+});
+input2.addEventListener('input', () => {
+  validatePIN();
+  moveToNextInput(input2, input3);
+});
+input3.addEventListener('input', () => {
+  validatePIN();
+  moveToNextInput(input3, input4);
+});
+input4.addEventListener('input', () => {
+  validatePIN();
+  moveToNextInput(input4, subscribeBtn);
+});
+
+const inputElement = document.getElementById('maskedInput');
+
+inputElement.addEventListener('input', function(event) {
+  const doneNum = document.getElementById('done');
+  const phoneNumberFormBtn = document.getElementById('phoneNumberFormBtn');
+  const input = event.target.value.replace(/\D/g, '');
+  const formattedValue = formatNumber(input);
+
+  event.target.value = formattedValue;
+
+  if (input.length > 10) {
+    doneNum.style.display = 'flex';
+    phoneNumberFormBtn.style.background = 'linear-gradient(180deg, #32E28E 0%, #19C974 100%)';
+    phoneNumberFormBtn.disabled = false;
+  } else {
+    doneNum.style.display = 'none';
+    phoneNumberFormBtn.style.background = 'linear-gradient(180deg, #797C7B 0%, #797C7B 100%);';
+    phoneNumberFormBtn.disabled = true;
+  }
+});
+
+function formatNumber(input) {
+  const regex = /^(\d{0,3})(\d{0,7})$/;
+  const match = input.match(regex);
+
+  if (match) {
+    const formattedValue = match.slice(1).filter(Boolean).join('-');
+    return formattedValue;
+  } else {
+    return input.slice(0, 10);
+  }
+}
+
+
